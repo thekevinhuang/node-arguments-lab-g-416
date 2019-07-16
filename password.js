6b8c5bf20475119fe3fa83a50563e746fcfefe77
Kevin Huang <yellow.khaos@gmail.com>
2019-07-16 22:09:45 +0000

    Automatically backed up by Learn

--- password.js
+++ password.js
@@ -1,6 +1,6 @@
 function generatePassword(length) {
   let password = ""
   for (i=0; i < length; i++) {
-    password = password + "Math.floor(Math.random()*10)"
+    password = password + `${Math.floor(Math.random()*10)}`
   }
 }
